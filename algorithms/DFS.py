2683feea26d0c16a8a7935548fa6321068344536
Roberto Sanchez Santoyo <robertosanchezsantoyo@gmail.com>
2026-05-26 13:39:12 -0600

    Add DFS algorithm and comments

--- algorithms/DFS.py
+++ algorithms/DFS.py
@@ -1,1 +1,22 @@
-def dfs():
+def dfs(graph, start ,goal):
+    stack = [(start, [start])]
+    visited = set()
+    explored = []
+
+    while stack:
+        node, path = stack.pop()
+
+        if node in visited:
+            continue
+
+        visited.add(node)
+        explored.append(node)
+
+        if node == goal:
+            return path, explored
+
+        for neighbor, cost in graph[node]:
+            if neighbor not in visited:
+                stack.append((neighbor, path + neighbor))
+
+    return None, explored
--- utils/__init__.py
+++ utils/__init__.py
@@ -4,7 +4,7 @@ from utils.graph import build_graph, read_graph
 # Algorithms
 from algorithms.BFS import bfs
 from algorithms.UCS import ucs
-# from algorithms.DFS import dfs
+from algorithms.DFS import dfs
 # from algorithms.DLS import dls
 # from algorithms.IDS import ids
 # from algorithms.Greedy_BFS import greedy_bfs
