ce22f26d45d0a2b2d7f777e28030a345ccb1fc10
Roberto Sanchez Santoyo <robertosanchezsantoyo@gmail.com>
2026-05-26 23:20:30 -0600

    Add IDS algorithm

--- algorithms/IDS.py
+++ algorithms/IDS.py
@@ -1,1 +1,18 @@
-def ids():
+from algorithms.DLS import dls
+
+# Search Algorithm: Iterative deepening search
+def ids(graph, start, goal):
+    explored_all = []
+    limit = 0
+
+    while True:
+        path, explored = dls(graph, start, goal, limit)
+        explored_all.extend(explored)
+
+        if path is not None:
+            return path, explored_all
+
+        if len(explored) == 0:
+            return None, explored_all
+
+        limit += 1
--- utils/__init__.py
+++ utils/__init__.py
@@ -6,6 +6,6 @@ from algorithms.BFS import bfs
 from algorithms.UCS import ucs
 from algorithms.DFS import dfs
 from algorithms.DLS import dls
-# from algorithms.IDS import ids
+from algorithms.IDS import ids
 # from algorithms.Greedy_BFS import greedy_bfs
 # from algorithms.A_star import a_star
